3118fa9d4d2cba53232d3bd788acf15ec9536433
Suit <suit387@gmail.com>
2017-12-08 13:14:58 +0500

    Fixed uptime

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const client = new Discord.Client();
 const env = require("./token.json")
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === '//hello') {
-    msg.reply("Hi, I am Valiant. I am currently in development.");
-  }
-  if (msg.content === "//uptime")
-    function format(seconds){
-        function pad(s){
-        return (s < 10 ? '0' : '') + s;
-        }
-        const hours = Math.floor(seconds / (60*60));
-        const minutes = Math.floor(seconds % (60*60) / 60);
-        const seconds = Math.floor(seconds % 60);
-    
-        return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    if (msg.content === '//hello') {
+        msg.reply("Hi, I am Valiant. I am currently in development.");
     }
-    
-    const uptime = process.uptime();
-    msg.reply(`${format(uptime)}`);
+    if (msg.content === "//uptime") {
+        function format(sec){
+            function pad(s){
+            return (s < 10 ? '0' : '') + s;
+            }
+            let hours = Math.floor(sec / (60*60));
+            let minutes = Math.floor(sec % (60*60) / 60);
+            let seconds = Math.floor(sec % 60);
+        
+            return `${pad(hours)} hours ${pad(minutes)} minutes ${pad(seconds)} seconds`;
+        }
+        
+        const uptime = process.uptime();
+        msg.reply(`${format(uptime)}`);
+    }
 });
 
 client.login(env.token);
