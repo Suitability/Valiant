d4fcdc2e2c083bef4ae310f2287e96b96b956d93
Suit <suit387@gmail.com>
2017-12-08 13:09:07 +0500

    Uptime

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ client.on('message', msg => {
   if (msg.content === '//hello') {
     msg.reply("Hi, I am Valiant. I am currently in development.");
   }
+  if (msg.content === "//uptime")
+    function format(seconds){
+        function pad(s){
+        return (s < 10 ? '0' : '') + s;
+        }
+        const hours = Math.floor(seconds / (60*60));
+        const minutes = Math.floor(seconds % (60*60) / 60);
+        const seconds = Math.floor(seconds % 60);
+    
+        return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    }
+    
+    const uptime = process.uptime();
+    msg.reply(`${format(uptime)}`);
 });
 
 client.login(env.token);
